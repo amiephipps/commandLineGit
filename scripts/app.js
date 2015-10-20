43fa4d6abc38caa0c47bd6a474b30fa14d833054
Amie Phipps <amiejoyphipps@gmail.com>
2015-10-20 11:48:32 -0400

    this is me commiting on my own

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,15 @@ for (var i = Things.length - 1; i >= 0; i--) {
 for (var i = Things.length - 1; i >= 0; i--) {
 	console.log(i);
 };
+for (var i = Things.length - 1; i >= 0; i--) {
+	console.log(i);
+};
+for (var i = Things.length - 1; i >= 0; i--) {
+	console.log(i);
+};
+for (var i = Things.length - 1; i >= 0; i--) {
+	console.log(i);
+};
+for (var i = Things.length - 1; i >= 0; i--) {
+	console.log(i);
+};
